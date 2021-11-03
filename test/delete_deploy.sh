1f58ef1a5e1c3d533ff08d65ce498ea5d10413c4
yongckim <zxcv9203@naver.com>
2021-11-04 02:51:39 +0900

    grade CrashLoopBackOff reason find

---
+++ test/delete_deploy.sh
@@ -0,0 +1,4 @@
+kubectl delete -f mariadb-pv.yml
+kubectl delete -f mariadb-deploy.yml
+kubectl delete -f grade-deploy.yml
+kubectl delete -f interact-deploy.yml
---
+++ test/setup.sh
@@ -0,0 +1,4 @@
+kubectl apply -f mariadb-pv.yml
+kubectl apply -f mariadb-deploy.yml
+kubectl apply -f grade-deploy.yml
+kubectl apply -f interact-deploy.yml
