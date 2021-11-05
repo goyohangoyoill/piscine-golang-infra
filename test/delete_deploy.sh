613714e035a136e3a88560cde7f5ec91bd385772
yongckim <zxcv9203@naver.com>
2021-11-05 14:51:05 +0900

    infra intstall automation

--- test/delete_deploy.sh
+++ test/delete_deploy.sh
@@ -1,4 +1,12 @@
-kubectl delete -f mariadb-pv.yml
-kubectl delete -f mariadb-deploy.yml
-kubectl delete -f grade-deploy.yml
-kubectl delete -f interact-deploy.yml
+#!/bin/bash
+PWFILE="./db/password.txt"
+DB="./db/maridb-deploy.yml"
+PV="./db/mariadb-pv.yml"
+GRADE="grade-deploy.yml"
+INTERACT="interact-deploy.yml"
+
+kubectl delete -f ${PV}
+kubectl delete -f ${DB}
+kubectl delete -f ${GRADE}
+kubectl delete -f ${INTERACT}
+kubectl delete secret mysql-password
--- test/setup.sh
+++ test/setup.sh
@@ -1,4 +1,17 @@
-kubectl apply -f mariadb-pv.yml
-kubectl apply -f mariadb-deploy.yml
-kubectl apply -f grade-deploy.yml
-kubectl apply -f interact-deploy.yml
+#!/bin/bash
+PWFILE="./db/password.txt"
+DB="./db/mariadb-deploy.yml"
+PV="./db/mariadb-pv.yml"
+GRADE="./grade/grade-deploy.yml"
+INTERACT="./interact/interact-deploy.yml"
+
+if [[ -e ${PWFILE} ]] ; then
+	kubectl create secret generic mysql-password --from-file=${PWFILE}
+else
+	echo "패스워드 파일을 추가해주세요"
+fi
+
+kubectl apply -f ${PV}
+kubectl apply -f ${DB}
+kubectl apply -f ${GRADE}
+kubectl apply -f ${INTERACT}
